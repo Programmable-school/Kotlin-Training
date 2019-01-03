e18fe5d97056ad448be38db5e1b4e296afd42ead
Shohei Nakagawa <hukusuke1007@gmail.com>
2019-01-03 23:25:33 +0900

    updated

---
+++ Lesson/src/Lesson.kt
@@ -0,0 +1,103 @@
+package lesson
+import java.util.*
+
+fun main(args: Array<String>) {
+    println("---- Lesson_HelloWorld ----")
+    lessonHelloWorld()
+    println("------------------\n")
+
+    println("---- Lesson_VarVal ----")
+    lessonVarVal()
+    println("------------------\n")
+
+    println("---- Lesson_PriAny ----")
+    lessonPriAny()
+    println("------------------\n")
+}
+
+/* Hello! World.を表示 */
+fun lessonHelloWorld() {
+    println("Hello! world.") // Hello!. world.
+}
+
+/* 変数と定数 */
+fun lessonVarVal() {
+    // 変数
+    var count: Int = 1
+    count += 1
+
+    var message: String = ""
+    message = "Lesson ${count}"
+    println(message)    // Lesson 2
+
+    // 定数
+    val name: String = "山田太郎"
+    // name = "Yamada Taro" // 定数のためビルドエラーになる
+    println(name)       // 山田太郎
+}
+
+/* プリミティブ型、Any型  */
+fun lessonPriAny() {
+    val numInt: Int = 10                // 32ビット符号付き整数
+    val numLong: Long = 10000000000000L // 64ビット浮動小数点数
+    val numDouble: Double = 10.123      // 64ビット浮動小数点数
+    val numFloat: Float = 10.123f       // 32ビット浮動小数点数
+    val numByte: Byte = 0xF             // 8ビット符号付き整数
+    val numShort: Short = 10000         // 16ビット符号付き整数
+    val str: String = "文字列"           // 文字列型
+    val char: Char = '文'               // １文字の文字型
+    val isActive: Boolean = true        // Boolean型（trueかfalse）
+    println(numInt)                 // 10
+    println(numLong)                // 10000000000000
+    println(numDouble)              // 10.123
+    println(numFloat)               // 10.123
+    println(numByte)                // 15
+    println(numShort)               // 10000
+    println(str)                    // 文字列
+    println(char)                   // 文
+    println(isActive)               // true
+
+    var variable: Any
+    variable = 1
+    println("number ${variable}")   // number 1
+
+    variable = "文字列"
+    println("String ${variable}")   // String 文字列
+
+    variable = false
+    println("Boolean ${variable}")  // Boolean false
+
+    variable = Date()
+    println("class ${variable}")    // class Thu Jan 03 23:22:20 JST 2019
+}
+/* Optional型 */
+/* null安全 */
+/* 演算子 */
+/* コレクション */
+/* 配列 */
+/* if分 */
+/* 比較演算子 */
+/* AND・OR */
+/* 三項演算子 */
+/* switch文 */
+/* when文 */
+/* while文 */
+/* for文 */
+/* 関数 */
+/* Enum */
+/* 構造体 */
+/* クラス */
+/* 継承 */
+/* インターフェース */
+/* 抽象クラス */
+/* 値渡し参照渡し */
+/* キャスト変換 */
+/* 日付関数（Date） */
+/* 数値計算（Math） */
+/* objectとcompanion object */
+/* 拡張関数 */
+/* ジェネリクス */
+/* ラムダ式 */
+/* コールバック */
+/* コルーチン */
+/* ReactiveX */
--- README.md
+++ README.md
@@ -1,1 +1,41 @@
 # Kotlin-Training
+
+> 初学者向け Kotlin構文理解のためのソースコード
+## 開発環境
+- IntelliJ IDEA 2018.3以上
+- Kotlin 1.3以上
+
+IntelliJ IDEAの公式サイトでIntelliJ IDEA Community（無償版）をダウンロードしてインストールしてください。<br> 
+[IntelliJ IDEA](https://www.jetbrains.com/idea/)<br>
+
+## 手順
+IntelliJ IDEAを利用してコーディングします。
+
+### 環境構築
+以下の手順を実行して
+1. IntelliJ IDEAを起動
+2. Create New Projectを選択
+3. KotlinのKotlin/JVMを選択してNextを選択
+4. Project nameで好きな名前に変更してFinishを選択（エディタが開くまで数秒かかります）
+
+### Hello! world.を表示する
+さきほど作成したのIntelliJ IDEAのプロジェクト内で、左側のメニューの「src」を右クリックより<br>
+「New」->「Kotlin File/Class」を選択してファイル名を「Lesson」にしてOKを選択して作成します。<br>
+<br>
+
+作成されたLesson.ktで以下のように実装してください。
+```kotlin
+package lesson
+
+fun main(args: Array<String>) {
+    /* Hello! World.を表示 */
+    println("Hello! world.") // Hello!. world.
+}
+```
+エディタツールバー -> Run -> Runで実行します。<br>
+またショートカットキーで「control + R」で実行できます（便利なのでオススメ）。<br>
+<br>
+実行するとエディタ下部の画面に "Hello world!"が表示されます。<br>
+<br>
+以上です。<br>
+以降、随時コンテンツを追加していきます。
