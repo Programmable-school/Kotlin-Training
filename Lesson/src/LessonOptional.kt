348cd4a6f37531bba769f2001f423fba49dc9238
Shohei Nakagawa <hukusuke1007@gmail.com>
2019-01-25 09:49:10 +0900

    updated

--- Lesson/src/LessonOptional.kt
+++ Lesson/src/LessonOptional.kt
@@ -1,13 +1,12 @@
 package lesson
 
 /* Optional型 */
+// Optional型の場合はnullの場合があるかも？という意味で?をつける。
 fun lessonOptional() {
     var strA: String? = null
     // var strA: String = null  // Optional型以外でnullを入れるとコンパイルエラーになる
     // println(strA.length)     // strAがnullの場合があるためコンパイルエラーになる
     println(strA)               // null
-
-    // Optional型の場合はnullの場合があるかも？という意味で?をつける。
     println(strA?.length)       // null
 
     strA = "hogehoge"
